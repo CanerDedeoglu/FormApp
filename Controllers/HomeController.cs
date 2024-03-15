21c3b32a79a7ef54bfa35875c398c8f2b8c2d6f1
Caner Dedeoglu <070caner4@gmail.com>
2024-03-16 02:21:35 +0300

    create_form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,29 +14,48 @@ public class HomeController : Controller
 
     }
 
-    public IActionResult Index(string searchstring, string category)
+    public IActionResult Index(string searchString, string category)
     {
         var product = Repository.Products;
 
-        if(!String.IsNullOrEmpty(searchstring))
+        if (!String.IsNullOrEmpty(searchString))
         {
-            ViewBag.SearchString = searchstring;
-            product = product.Where(p =>p.BookName.ToLower().Contains(searchstring)).ToList();
+
+            ViewBag.SearchString = searchString;
+            product = product.Where(p => p.BookName.ToLower().Contains(searchString)).ToList();
         }
-        if(!String.IsNullOrEmpty(category) && category != "0")
+
+        if (!String.IsNullOrEmpty(category) && category != "0")
         {
-            product = product.Where(p=>p.CategoryId == int.Parse(category)).ToList();
+            product = product.Where(p => p.CategoryId == int.Parse(category)).ToList();
         }
-        ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId","CategoryName");
 
+        var model = new ProductViewModel
+        {
+            Products = product,
+            Categories = Repository.Categories,
+            SelectedCategory = category
+        };
 
-        return View(product);
-        
-
+        return View(model);
     }
     public IActionResult Admin()
     {
         return View(Repository.Products);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+       ViewBag.Categories = Repository.Categories;
+       return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {
+
+        return View();
     }
 
 }
